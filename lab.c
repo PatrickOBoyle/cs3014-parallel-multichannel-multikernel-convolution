3ba341f6bdfc0146de1516a9248db1f5b26a35aa
Patrick O'Boyle <oboylepa@tcd.ie>
2017-02-26 15:55:29 +0000

    Remove un needed files

--- lab.c
+++
@@ -1,7 +0,0 @@
-// To compile with SSE on stoker you should:
-#include<x86intrin.h> in your C/C++ code
-
-//compile your program with: gcc -O3 -msse4 file.c
-// To comile with OpenMP, add the flag -fopenmp.
-
-// BEGIN ASSIGNMENT
