7c59551ec2caf7de8b7c584184e8a190eb95b7d2
Shane Fay <faysh@tcd.ie>
2017-03-22 11:42:43 +0000

    change to iterate over kernels and updated run script to uses gregg's test

--- conv-harness.c
+++ conv-harness.c
@@ -251,11 +251,11 @@ void team_conv(float *** image, float **** kernels, float *** output,
 
 
   #pragma omp parallel for private(w, h, m, c, x, y) shared(output, image, kernels) if(width * height > 50000)
-  for ( w = 0; w < width; w++ )
+  for ( m = 0; m < nkernels; m++ )
   {
     for ( h = 0; h < height; h++ )
     {
-      for ( m = 0; m < nkernels; m++ )
+      for ( w = 0; w < width; w++ )
       {
         double sum = 0.0;
         for ( c = 0; c < nchannels; c++ )
--- runner.py
+++ runner.py
@@ -3,26 +3,9 @@ from subprocess import call
 call(["gcc-6", "-O3", "-fopenmp", "-msse4", "conv-harness.c"])
 call(["echo", "GCC finished"])
 
-call(["echo", "Running 100x100 tests..."])
-call(["./a.out", "100", "100", "1", "1", "1"])
-call(["./a.out", "100", "100", "1", "3", "3"])
-call(["./a.out", "100", "100", "3", "3", "3"])
-call(["./a.out", "100", "100", "3", "10", "10"])
+call(["echo", "Running tests..."])
+call(["./a.out", "128", "128", "5", "32", "100"])
+call(["./a.out", "32", "32", "3", "64", "1024"])
+call(["./a.out", "255", "255", "1", "63", "127"])
+call(["./a.out", "192", "912", "7", "1", "512"])
 
-call(["echo", "Running 300x300 tests..."])
-call(["./a.out", "300", "300", "1", "1", "1"])
-call(["./a.out", "300", "300", "1", "3", "3"])
-call(["./a.out", "300", "300", "3", "3", "3"])
-call(["./a.out", "300", "300", "3", "10", "10"])
-
-call(["echo", "Running 500x500 tests..."])
-call(["./a.out", "500", "500", "3", "10", "10"])
-
-call(["echo", "Running 1000x1000 tests..."])
-call(["./a.out", "1000", "1000", "3", "10", "10"])
-
-call(["echo", "Running 2000x2000 tests..."])
-call(["./a.out", "2000", "2000", "3", "10", "10"])
-
-call(["echo", "Running 3000x3000 tests..."])
-call(["./a.out", "3000", "3000", "3", "10", "10"])
